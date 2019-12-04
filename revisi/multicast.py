52d800aac78b11440e815d49961dcae11fab7b2f
raldo <raldo0104@gmail.com>
2019-12-04 15:14:14 +0700

    revisi jadi 1 program

---
+++ revisi/multicast.py
@@ -0,0 +1,72 @@
+import socket
+import struct
+import sys
+import threading
+
+def multicast_recv():
+	groupip='224.3.29.73'
+	groupport=10000
+	multicast_group = groupip
+	server_address = ('', groupport)
+	# Create the socket
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+	# Bind to the server address
+	sock.bind(server_address)
+
+	# Tell the operating system to add the socket to the multicast group
+	# on all interfaces.
+	group = socket.inet_aton(multicast_group)
+	mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+	sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+	while True:
+		sock.settimeout(None)
+		data, address = sock.recvfrom(1024)
+		print('received %s bytes from %s' % (len(data), address))
+		print(data.decode())
+		print('sending acknowledgement to', address)
+		sock.sendto('theack'.encode(), address)
+
+def multicast_send():
+	message = 'Pesan penting untukmu'
+	multicast_group = ('224.3.29.73', 10000)
+
+	# Create the datagram socket
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+	# Set a timeout so the socket does not block indefinitely when trying
+	# to receive data.
+	sock.settimeout(60)
+
+	ttl = struct.pack('b', 1)
+	sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+	try:
+
+	    # Send data to the multicast group
+	    print ('sending "%s"' % message)
+	    sent = sock.sendto(message.encode(), multicast_group)
+
+	    # Look for responses from all recipients
+	    while True:
+	        print ('waiting to receive')
+	        try:
+	            data, server = sock.recvfrom(16)
+	        except socket.timeout:
+	            print ('timed out, no more responses')
+	            break
+	        else:
+	            print ('received "%s" from %s' % (data, server))
+	            break
+
+	finally:
+	    print ('closing socket')
+	    sock.close()
+
+if __name__ == "__main__":
+	x = threading.Thread(target=multicast_recv, args=())
+	x.start()
+	while True:
+		command=input("enter 1 to send: ")
+		if command=='1':
+			multicast_send()
---
+++ revisi/multicast_send.py
@@ -0,0 +1,42 @@
+import socket
+import struct
+import sys
+
+# groupip=str(sys.argv[1])
+# groupport=int(sys.argv[2])
+# destination=input('destination: ')
+# iam=input("who am i? ")
+message = 'D TO A'
+multicast_group = ('224.3.29.73', 10000)
+
+# Create the datagram socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Set a timeout so the socket does not block indefinitely when trying
+# to receive data.
+sock.settimeout(60)
+
+ttl = struct.pack('b', 1)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+
+try:
+
+    # Send data to the multicast group
+    print ('sending "%s"' % message)
+    sent = sock.sendto(message.encode(), multicast_group)
+
+    # Look for responses from all recipients
+    while True:
+        print ('waiting to receive')
+        try:
+            data, server = sock.recvfrom(16)
+        except socket.timeout:
+            print ('timed out, no more responses')
+            break
+        else:
+            print ('received "%s" from %s' % (data, server))
+            break
+
+finally:
+    print ('closing socket')
+    sock.close()
