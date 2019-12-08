4d30a2f6a843d56394a6c55faecae7089e5c336f
Ibrahim Tamtama Adi <ibrahimtamtama17@gmail.com>
2019-12-09 00:20:02 +0700

    tamtam ngedit dikit aja nih, maap maap

--- revisi/multicast.py
+++ revisi/multicast.py
@@ -5,13 +5,13 @@ import time
 import threading
 import hashlib
 
-max_buffer = 5
+max_hop = 5
+hashbuffer = []
+msgbuffer = []
+groupip='224.3.29.73'
+groupport=10000
 
 def multicast_recv(a,iam):
-	hashbuffer = []
-	msgbuffer = []
-	groupip='224.3.29.73'
-	groupport=10000
 	multicast_group = groupip
 	server_address = ('', groupport)
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -25,80 +25,64 @@ def multicast_recv(a,iam):
 		sock.settimeout(None)
 		data, address = sock.recvfrom(1024)
 		print('\nreceived %s bytes from %s' % (len(data), address))
-		print(data.decode())
+		# print(data.decode())
 		pesan=data.decode().split(";")
-		if str(pesan[2])==iam:
-			continue
-		elif str(pesan[3] == iam):
+		if str(pesan[3]) == iam:
 			print("ada pesan!")
 			print(str(pesan[0]))
 		#kalo belum melebihi hop
-		elif int(pesan[1]) < max_buffer:
+		elif int(pesan[1]) < max_hop:
 			#kalo pesannya belum ada di buffer
 			if str(pesan[4]) not in hashbuffer:
-				kirim = str(pesan[0]) + ";" + str(int(pesan[1]) + 1) + ";" + str(pesan[2]) + str(pesan[3]) + str(pesan[4])
+				kirim = data.decode()
 				hashbuffer.append(str(pesan[4]))
 				msgbuffer.append(kirim)
-				for i in msgbuffer:
-					send_only = threading.Thread(target=multicast_send_only, args=(1,i))
-					send_only.start()
 			#drop kalo udah punya messagenya di buffer
 			else:
 				print("message already exists on the buffer, dropping message..")
 		else:
-			print("dropping packets due to maximum amount of buffer.")
+			print("dropping packets due to maximum amount of hop.")
 		# print('sending acknowledgement to', address)
 		# sock.sendto('theack'.encode(), address)
 
-def multicast_send_only(a,pesan):
-	time.sleep(1)
+def multicast_send_only(pesan):
 	message = pesan
-	multicast_group = ('224.3.29.73', 10000)
+	multicast_group = (groupip, groupport)
 
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-	sock.settimeout(60)
 
 	ttl = struct.pack('b', 1)
 	sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 	sent = sock.sendto(message.encode(), multicast_group)
-	print(sent)
+	# print(sent)
 
-def multicast_send(a,pesan):
-	message = pesan
-	multicast_group = ('224.3.29.73', 10000)
+def multicast_buffering():
+	while True:
+		time.sleep(5)
+		lala = 0
+		for x in msgbuffer:
+			msg=x.split(";")
+			if int(msg[1])<max_hop:
+				hopnya = int(msg[1]) + 1
+				kirim = str(msg[0]) + ";" + str(hopnya) + ";" + str(msg[2]) + ";" + str(msg[3]) + ";" + str(msg[4])
+				multicast_send_only(kirim)
+				msgbuffer[int(lala)]=kirim
+			lala = int(lala)+1
 
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-	sock.settimeout(60)
-
-	ttl = struct.pack('b', 1)
-	sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
-	try:
-
-		print ('sending "%s"' % message)
-		sent = sock.sendto(message.encode(), multicast_group)
-		print(sent)
-		
-		while True:
-			print ('waiting to receive')
-			try:
-				data, server = sock.recvfrom(16)
-			except socket.timeout:
-				print ('timed out, no more responses')
-				break
-			else:
-				print ('received "%s" from %s' % (data, server))
-				break
-
-	finally:
-		print ('closing socket')
-		sock.close()
+		po = 0
+		for i in msgbuffer:
+			psn=i.split(";")
+			if int(psn[1])>=max_hop:
+				del msgbuffer[po]
+				del hashbuffer[po]
+			po = int(po)+1
 
 if __name__ == "__main__":
 	iam=input("who are you? ")
 	recv = threading.Thread(target=multicast_recv, args=(1,iam))
 	recv.start()
+	buff = threading.Thread(target=multicast_buffering, args=())
+	buff.start()
 	while True:
 		command=input("inputkan 1 untuk mengirim >> ")
 		if command=='1':
@@ -109,6 +93,4 @@ if __name__ == "__main__":
 			hashhex = thehash.hexdigest()
 			
 			pesan = pesanku + ";0;" + str(iam) + ";" + str(receiver) + ";" + str(hashhex)
-			send = threading.Thread(target=multicast_send_only, args=(1,pesan))
-			send.start()
-			# multicast_send('Pesan penting untukmu;0')
+			multicast_send_only(pesan)
